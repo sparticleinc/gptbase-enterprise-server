5775e7689218c1a3cb3946cd5b38aef1b852d8fe
陈晨 <373471664@qq.com>
2023-11-24 10:10:44 +0800

    feat: remove remark

--- gptbase_enterprise/endpoints/environmental_data.py
+++ gptbase_enterprise/endpoints/environmental_data.py
@@ -8,21 +8,18 @@ from gptbase_enterprise.schemata import EnvironmentDataIn
 router = APIRouter(prefix='/api/v1/environmental_data', tags=['Environmental data'])
 
 
-# 获取环境配置
 @router.get('')
 async def get_environmental_data():
     environmental_data = await EnvironmentData.filter(deleted_at__isnull=True).all()
     return environmental_data
 
 
-# 新增环境配置
 @router.post('')
 async def create_environmental_data(environmental_data: EnvironmentDataIn):
     environmental_data_obj = await EnvironmentData.create(**environmental_data.dict(exclude_unset=True))
     return environmental_data_obj
 
 
-# 修改环境配置
 @router.put('/{environmental_data_id}')
 async def update_environmental_data(environmental_data_id: UUID, environmental_data: EnvironmentDataIn):
     environmental_data_obj = await EnvironmentData.get_or_none(id=environmental_data_id, deleted_at__isnull=True)
@@ -35,7 +32,6 @@ async def update_environmental_data(environmental_data_id: UUID, environmental_d
     return await EnvironmentData.get(id=environmental_data_id)
 
 
-# 删除环境配置
 @router.delete('/{environmental_data_id}', status_code=204)
 async def delete_environmental_data(environmental_data_id: UUID):
     environmental_data_obj = await EnvironmentData.get_or_none(id=environmental_data_id, deleted_at__isnull=True)
